0683c4df28424ed6bed6d1a40454f53298f85cb2
abhishekjain2604 <jainabhishek2604@gmail.com>
2018-12-02 13:21:30 +0530

    Defined basic structure of the transport

---
+++ index.js
@@ -0,0 +1,25 @@
+const Transport = require('winston-transport');
+
+class SentryTransport extends Transport {
+  constructor(opts) {
+    super(opts);
+
+    //
+    // Consume any custom options here. e.g.:
+    // - Connection information for databases
+    // - Authentication information for APIs (e.g. loggly, papertrail, 
+    //   logentries, etc.).
+    //
+  }
+
+  log(info, callback) {
+    setImmediate(() => {
+      this.emit('logged', info);
+    });
+
+    // Perform the writing to the remote service
+    callback();
+  }
+};
+
+module.exports = SentryTransport;
